3d21733ccae8585cc6e00540092c4107345479c2
beffcake007 <1054390839@qq.com>
2018-07-27 19:34:37 +0800

    wechat product init finished

---
+++ src/main/java/com/fanye/sell/WebConfig.java
@@ -0,0 +1,26 @@
+package com.fanye.sell;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * 解决vue跨域请求的问题
+ */
+@Configuration
+public class WebConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "GET", "POST")
+                .allowedHeaders("*")
+                .exposedHeaders("access-control-allow-headers",
+                        "access-control-allow-methods",
+                        "access-control-allow-origin",
+                        "access-control-max-age",
+                        "X-Frame-Options")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
---
+++ src/main/java/com/fanye/sell/service/ProductService.java
@@ -0,0 +1,27 @@
+package com.fanye.sell.service;
+
+import com.fanye.sell.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ProductService {
+
+    ProductInfo findById(String productId);
+
+    /**
+     * 查询所有在架的商品列表
+     *
+     * @return
+     */
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+
+    //加库存
+
+    //减库存
+}
